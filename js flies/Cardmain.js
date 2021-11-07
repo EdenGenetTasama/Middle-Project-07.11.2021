fc8d42e588a8113576641d1de689c96a7629901c
EdenGenetTasama <edentasama19@gmail.com>
2021-11-07 20:59:39 +0200

    second Commit

--- js flies/Cardmain.js
+++ js flies/Cardmain.js
@@ -1,1 +1,31 @@
-const apiMainURL = "moviesmern.herokuapp.com";
+const apiMainURL = "https://moviesmern.herokuapp.com/";
+
+
+
+async function getAllMovies(api,src) {
+  try {
+      return await fetch(`${api}${src}`).then((res)=>res.json())
+  } catch (error) {
+    return error;
+  }
+}
+
+
+
+function runAllArrayOfObject(arrayOfObject) {
+
+    for (const item of arrayOfObject.data) {
+        console.log(item);
+        divOfInfo.innerHTML += `<article class="cardClass">
+        <img src="${item.image}" id="imgOfInfo">
+        <h1 class="nameTitle">${item.movieName}</h1>
+        <p>${item.rating}</p>
+        </article>
+        `;
+    }
+}
+
+
+
+
+getAllMovies(apiMainURL , "movies/all").then((res)=>runAllArrayOfObject(res));
---
+++ js flies/tablePage.js
@@ -0,0 +1,43 @@
+const apiMainURLTablePage = "https://moviesmern.herokuapp.com/";
+let tableID = document.getElementById("tableID");
+
+function loadingGif() {
+    container.innerHTML += `<img src="../images/loading gif.gif" id="gifId">`;
+}
+
+function stopLoading() {
+  gifId.style.display = "none";
+}
+
+async function getAllMovies(api, src) {
+  try {
+    // loadingGif()
+    return await fetch(`${api}${src}`).then((res) => res.json());
+  } catch (error) {
+    return error;
+  }
+}
+
+function tableShowFunction(arrayOfObject) {
+  for (const item of arrayOfObject.data) {
+    console.log(item);
+    tableID.innerHTML += `
+    <tr>
+        <td>${item.movieName}</td>
+        <td>${item.image}</td><br>
+        <td>${item.linkToMovie}</td>
+        <td>${item.rating}</td>
+        <td>${item.synopsis}</td><br>
+        <td>${item._id}</td>
+        <td>${item.date}</td>
+    </tr>
+        
+        `;
+  }
+}
+
+
+getAllMovies(apiMainURLTablePage, "movies/all")
+  .then((res) => tableShowFunction(res))
+  .catch((rej) => console.log(rej))
+//   .finally(stopLoading());
