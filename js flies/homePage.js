a09546691756705f660905112517a4adaa6bb839
EdenGenetTasama <edentasama19@gmail.com>
2022-01-17 18:29:00 +0200

    add image

--- js flies/homePage.js
+++ js flies/homePage.js
@@ -28,25 +28,25 @@ function showSlides() {
 divImageOnHomePage.innerHTML = 
 `
 <div class="divOfImageShow">
-<img src="../images2/funny.jpg">
+<img src="https://pbs.twimg.com/profile_images/1208234904405757953/mT0cFOVQ_400x400.jpg">
 <div class="hoverCap">Funny</div>
 </div>
 
 
 <div class="divOfImageShow">
-<img src="../images2/romantiv.jpg">
+<img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/tatb3-unit-10957-r-1634841828.jpg">
 <div class="hoverCap">Romantic</div>
 </div> 
 
 
 <div class="divOfImageShow">
-<img src="https://pbs.twimg.com/profile_images/1208234904405757953/mT0cFOVQ_400x400.jpg">
+<img src="https://see.news/wp-content/uploads/2021/09/daniel-craig-bond-1d24ba1.jpg">
 <div class="hoverCap">Action</div>
 
 </div>
 
 <div class="divOfImageShow">
-<img src="../פרוייקט אתר סרטים 7.11.2021/images2/doco.jpg">
+<img src="https://m.media-amazon.com/images/M/MV5BMTg3NzY4NTI3N15BMl5BanBnXkFtZTgwMzc0MDc4NDM@._V1_.jpg">
 <div class="hoverCap">Documentary</div>
 </div>
 `
